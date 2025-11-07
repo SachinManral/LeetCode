ba09538df5abf04a12a775e109c47dc8f77c101e
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-11-07 20:40:00 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String newStr = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String str = s.replaceAll("[^a-zA-Z1-9]", "").toLowerCase();
 
         int left = 0;
-        int right = newStr.length()-1;
+        int right = str.length()-1;
 
-        while(left < right) {
-            if(newStr.charAt(left) != newStr.charAt(right)) {
+        while(left<right) {
+            if(str.charAt(left) != str.charAt(right)) {
                 return false;
             }
             left++;
