2477cf1ba7a6b3ed091f0cdc3f197a14edabb20f
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-11-14 02:12:37 +0530

    Time: 3 ms (100%), Space: 175.3 MB (10.5%) - LeetHub

--- 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.java
@@ -1,24 +1,47 @@
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         Stack<Integer> pos = new Stack<>();
+//         Stack<Integer> neg = new Stack<>();
+
+//         for(int i=0; i<nums.length; i++) {
+//             if(nums[i] > 0) {
+//                 pos.push(nums[i]);
+//             }
+//             else {
+//                 neg.push(nums[i]);
+//             }
+//         }
+//         for(int i=nums.length-1; i>=0; i--) {
+//             if(i%2==0) {
+//                 nums[i] = pos.pop();
+//             } else {
+//                 nums[i] = neg.pop();
+//             }
+//         }
+
+//         return nums;
+//     }
+// }
+
+
+
+
 class Solution {
     public int[] rearrangeArray(int[] nums) {
-        Stack<Integer> pos = new Stack<>();
-        Stack<Integer> neg = new Stack<>();
+        int[] result = new int[nums.length];
+        int pos = 0; 
+        int neg = 1;
 
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] > 0) {
-                pos.push(nums[i]);
-            }
-            else {
-                neg.push(nums[i]);
-            }
-        }
-        for(int i=nums.length-1; i>=0; i--) {
-            if(i%2==0) {
-                nums[i] = pos.pop();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result[pos] = nums[i];
+                pos += 2;
             } else {
-                nums[i] = neg.pop();
+                result[neg] = nums[i];
+                neg += 2;
             }
         }
 
-        return nums;
+        return result;
     }
 }
