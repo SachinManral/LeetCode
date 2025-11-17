3fef4c22a3a7040c782bbc5015b268b783bcd5a0
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-11-17 21:10:40 +0530

    Time: 0 ms (100%), Space: 42.2 MB (9.15%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -1,24 +1,23 @@
 class Solution {
     static final int M = 1000000007;
-
     private long findPower(long a, long b) {
         if(b==0) {
             return 1;
         }
-        long half = findPower(a, b/2);
-        long result = (half*half) % M;
 
-        if(b % 2 == 1) {
+        long half = findPower(a, b/2);
+        long result = (half * half) % M;
+
+        if(b%2 == 1) {
             result = (result * a) % M;
         }
         return result;
     }
     public int countGoodNumbers(long n) {
-        long evenIndex = (n+1)/2;
-        long oddIndex = n/2;
+        long even = (n+1)/2;
+        long odd = n/2;
 
-        long result = (findPower(5, evenIndex) * findPower(4, oddIndex)) % M;
+        long result = (findPower(5, even) * findPower(4, odd)) % M;
         return (int) result;
-        
     }
 }
