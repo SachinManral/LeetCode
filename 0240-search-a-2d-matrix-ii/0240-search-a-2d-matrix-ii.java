893cd0edf37e29ee8b27b06cb2c90c33e832b983
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-11-19 12:57:28 +0530

    Time: 3 ms (97.1%), Space: 48.1 MB (18.63%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -1,20 +1,43 @@
+// class Solution {
+//     public boolean searchMatrix(int[][] matrix, int target) {
+//         int n = matrix.length;
+//         int m = matrix[0].length;
+
+//         int row = 0;
+//         int col = m - 1;
+
+//         while (row < n && col >= 0) {
+//             if (matrix[row][col] == target) {
+//                 return true;
+//             } else if (matrix[row][col] > target) {
+//                 col--;
+//             } else {
+//                 row++;
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int n = matrix.length;
-        int m = matrix[0].length;
+        return helper(matrix, 0, matrix[0].length - 1, target);
+    }
 
-        int row = 0;
-        int col = m - 1;
+    public boolean helper(int[][] arr, int row, int col, int target) {
+        int n = arr.length;
+        int m = arr[0].length;
 
-        while (row < n && col >= 0) {
-            if (matrix[row][col] == target) {
-                return true;
-            } else if (matrix[row][col] > target) {
-                col--;
-            } else {
-                row++;
-            }
+        if (row >= n || col < 0) return false;
+
+        if (arr[row][col] == target) return true;
+
+        if (arr[row][col] > target) {
+            return helper(arr, row, col - 1, target);
+        } else {
+            return helper(arr, row + 1, col, target);
         }
-        return false;
     }
 }
