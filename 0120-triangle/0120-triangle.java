db527e20603cb93f9361a7eddbf6cf83e19483f5
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-12-07 00:04:32 +0530

    Time: 5 ms (46.01%), Space: 45.6 MB (37.75%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,3 +1,45 @@
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         return helper(triangle, 0, 0);
+//     }
+//     private int helper(List<List<Integer>> triangle, int i, int j) {
+//         int n = triangle.size();
+//         if(i==n-1) return triangle.get(i).get(j);
+
+//         int down = triangle.get(i).get(j) + helper(triangle, i+1, j);
+//         int diag = triangle.get(i).get(j) + helper(triangle, i+1, j+1);
+
+//         return Math.min(down, diag);
+//     }
+// }
+
+
+
+
+// class Solution {
+//     public int minimumTotal(List<List<Integer>> triangle) {
+//         int n = triangle.size();
+//         int[][] dp = new int[n][n];
+//         for(int i=0; i<n; i++) {
+//             Arrays.fill(dp[i], -1);
+//         }
+//         return helper(triangle, 0, 0, dp);
+//     }
+//     private int helper(List<List<Integer>> triangle, int i, int j, int[][] dp) {
+//         int n = triangle.size();
+
+//         if(i==n-1) return triangle.get(i).get(j);
+//         if(dp[i][j] != -1) return dp[i][j];
+
+//         int down = triangle.get(i).get(j) + helper(triangle, i+1, j, dp);
+//         int diag = triangle.get(i).get(j) + helper(triangle, i+1, j+1, dp);
+
+//         return dp[i][j] = Math.min(down, diag);
+//     }
+// }
+
+
+
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
