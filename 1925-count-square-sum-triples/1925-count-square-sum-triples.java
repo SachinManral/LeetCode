6d75fb09225fbf8d6ce925788626bc7fe5bf0a03
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-12-08 16:03:22 +0530

    Time: 12 ms (64.09%), Space: 42.1 MB (37.58%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.java
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.java
@@ -1,12 +1,31 @@
+// class Solution {
+//     public int countTriples(int n) {
+//         int count = 0;
+//         for(int a=2; a<=n; a++) {
+//             for(int b=2; b<=n; b++) {
+//                 for(int c=3; c<=n; c++) {
+//                     if(((a*a)+(b*b)) == (c*c)){
+//                         count++;
+//                     }
+//                 }
+//             }
+//         }
+//         return count;
+//     }
+// }
+
+
+
+
 class Solution {
     public int countTriples(int n) {
         int count = 0;
-        for(int a=2; a<=n; a++) {
+        for(int a=1; a<=n; a++) {
             for(int b=2; b<=n; b++) {
-                for(int c=3; c<=n; c++) {
-                    if(((a*a)+(b*b)) == (c*c)){
-                        count++;
-                    }
+                int sum = (int)Math.sqrt((a*a)+(b*b));
+
+                if(sum*sum == (a*a)+(b*b) && sum<=n) {
+                    count++;
                 }
             }
         }
