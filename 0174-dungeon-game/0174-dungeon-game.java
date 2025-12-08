6b8d3d41ee46c2809f3926f44059c732b66a389d
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-12-08 11:24:44 +0530

    Time: 1 ms (69.6%), Space: 45.6 MB (18.16%) - LeetHub

--- 0174-dungeon-game/0174-dungeon-game.java
+++ 0174-dungeon-game/0174-dungeon-game.java
@@ -42,27 +42,92 @@
 
 
 
+// class Solution {
+//     public int calculateMinimumHP(int[][] d) {
+//         int n = d.length;
+//         int m = d[0].length;
+//         return solve(0, 0, d);
+//     }
+//     public int solve(int i, int j, int[][] d) {
+//         int n = d.length;
+//         int m = d[0].length;
+//         if(i>=n || j>=m) {
+//             return (int)1e9;
+//         }
+//         if(i==n-1&& j==m-1) {
+//             if(d[i][j] > 0) {
+//                 return 1;
+//             }else {
+//                 return Math.abs(d[i][j]) + 1;
+//             }
+//         }
+//         int down = solve(i+1, j, d);
+//         int right = solve(i, j+1, d);
+//         int need = Math.min(down, right) - d[i][j];
+        
+//         return (need <= 0) ? 1 : need;
+//     }
+// }
+
+
+
+
 class Solution {
     public int calculateMinimumHP(int[][] d) {
         int n = d.length;
         int m = d[0].length;
-        int[][] dp = new int[n][m];
-        dp[n-1][m-1] = Math.max(1, 1-d[n-1][m-1]);
-
-        for(int i=n-2; i>=0; i--) {
-            dp[i][m-1] = Math.max(1, dp[i+1][m-1] - d[i][m-1]);
+        Integer[][] dp = new Integer[n][m];
+        return solve(0, 0, d, dp);
+    }
+    public int solve(int i, int j, int[][] d, Integer[][] dp) {
+        int n = d.length;
+        int m = d[0].length;
+        if(i>=n || j>=m) {
+            return (int)1e9;
         }
-        for(int i=m-2; i>=0; i--) {
-            dp[n-1][i] = Math.max(1, dp[n-1][i+1] - d[n-1][i]);
-        }
-
-        for(int i=n-2; i>=0; i--) {
-            for(int j=m-2; j>=0; j--) {
-                int min = Math.min(dp[i+1][j], dp[i][j+1]);
-                dp[i][j] = Math.max(1, min-d[i][j]);
+        if(i==n-1&& j==m-1) {
+            if(d[i][j] > 0) {
+                return 1;
+            }else {
+                return Math.abs(d[i][j]) + 1;
             }
         }
-
-        return dp[0][0];
+        if(dp[i][j] != null) return dp[i][j];
+        int down = solve(i+1, j, d, dp);
+        int right = solve(i, j+1, d, dp);
+        int need = Math.min(down, right) - d[i][j];
+        
+        return dp[i][j] = (need <= 0) ? 1 : need;
     }
 }
+
+
+
+
+
+
+
+// class Solution {
+//     public int calculateMinimumHP(int[][] d) {
+//         int n = d.length;
+//         int m = d[0].length;
+//         int[][] dp = new int[n][m];
+//         dp[n-1][m-1] = Math.max(1, 1-d[n-1][m-1]);
+
+//         for(int i=n-2; i>=0; i--) {
+//             dp[i][m-1] = Math.max(1, dp[i+1][m-1] - d[i][m-1]);
+//         }
+//         for(int i=m-2; i>=0; i--) {
+//             dp[n-1][i] = Math.max(1, dp[n-1][i+1] - d[n-1][i]);
+//         }
+
+//         for(int i=n-2; i>=0; i--) {
+//             for(int j=m-2; j>=0; j--) {
+//                 int min = Math.min(dp[i+1][j], dp[i][j+1]);
+//                 dp[i][j] = Math.max(1, min-d[i][j]);
+//             }
+//         }
+
+//         return dp[0][0];
+//     }
+// }
