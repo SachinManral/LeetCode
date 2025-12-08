364e2be89e0fab57310883a99d201c7c28d041f0
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-12-08 12:01:10 +0530

    Time: 1 ms (99.81%), Space: 46.4 MB (15.52%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -16,47 +16,45 @@
 
 
 
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        Integer[][] dp = new Integer[n][n];
+        
+        return helper(triangle, 0, 0, dp);
+    }
+    private int helper(List<List<Integer>> triangle, int i, int j, Integer[][] dp) {
+        int n = triangle.size();
+
+        if(i==n-1) return triangle.get(i).get(j);
+        if(dp[i][j] != null) return dp[i][j];
+
+        int down = triangle.get(i).get(j) + helper(triangle, i+1, j, dp);
+        int diag = triangle.get(i).get(j) + helper(triangle, i+1, j+1, dp);
+
+        return dp[i][j] = Math.min(down, diag);
+    }
+}
+
+
+
 // class Solution {
 //     public int minimumTotal(List<List<Integer>> triangle) {
 //         int n = triangle.size();
 //         int[][] dp = new int[n][n];
+        
 //         for(int i=0; i<n; i++) {
-//             Arrays.fill(dp[i], -1);
+//             dp[n-1][i] = triangle.get(n-1).get(i);
 //         }
-//         return helper(triangle, 0, 0, dp);
-//     }
-//     private int helper(List<List<Integer>> triangle, int i, int j, int[][] dp) {
-//         int n = triangle.size();
 
-//         if(i==n-1) return triangle.get(i).get(j);
-//         if(dp[i][j] != -1) return dp[i][j];
+//         for(int i=n-2; i>=0; i--) {
+//             for(int j=i; j>=0; j--) {
+//                 int down = triangle.get(i).get(j)+dp[i+1][j];
+//                 int diag = triangle.get(i).get(j)+dp[i+1][j+1];
+//                 dp[i][j] = Math.min(down, diag);
 
-//         int down = triangle.get(i).get(j) + helper(triangle, i+1, j, dp);
-//         int diag = triangle.get(i).get(j) + helper(triangle, i+1, j+1, dp);
-
-//         return dp[i][j] = Math.min(down, diag);
+//             }
+//         } 
+//         return dp[0][0];
 //     }
 // }
-
-
-
-class Solution {
-    public int minimumTotal(List<List<Integer>> triangle) {
-        int n = triangle.size();
-        int[][] dp = new int[n][n];
-        
-        for(int i=0; i<n; i++) {
-            dp[n-1][i] = triangle.get(n-1).get(i);
-        }
-
-        for(int i=n-2; i>=0; i--) {
-            for(int j=i; j>=0; j--) {
-                int down = triangle.get(i).get(j)+dp[i+1][j];
-                int diag = triangle.get(i).get(j)+dp[i+1][j+1];
-                dp[i][j] = Math.min(down, diag);
-
-            }
-        } 
-        return dp[0][0];
-    }
-}
