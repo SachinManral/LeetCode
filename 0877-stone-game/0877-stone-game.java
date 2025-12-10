fdd1812a34cf40f1aee5705ed5662163a7857fc3
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-12-10 21:15:47 +0530

    Time: 0 ms (100%), Space: 43 MB (56.91%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -6,20 +6,58 @@
 
 
 
+// class Solution {
+//     public boolean stoneGame(int[] piles) {
+//         Arrays.sort(piles);
+//         int n = piles.length;
+//         int alice = 0;
+//         int bob = 0;
+
+//         for(int i=n-1; i>=0; i--) {
+//             if(i%2==1) {
+//                 alice+=piles[i];
+//             }else {
+//                 bob+=piles[i];
+//             }
+//         }
+//         return (alice>bob)?true:false;
+//     }
+// }
+
+
+
+
+
 class Solution {
     public boolean stoneGame(int[] piles) {
-        Arrays.sort(piles);
-        int n = piles.length;
+        int n = piles.length-1;
         int alice = 0;
         int bob = 0;
+        int i = 0;
+        boolean turn = true;
 
-        for(int i=n-1; i>=0; i--) {
-            if(i%2==1) {
-                alice+=piles[i];
+        while(i<n) {
+            if(turn) {
+                if(piles[i]>piles[n]) {
+                    alice+=piles[i];
+                    i++;
+                }else {
+                    alice+=piles[n];
+                    n--;
+                }
             }else {
-                bob+=piles[i];
+                if(piles[i]>piles[n]) {
+                    alice+=piles[i];
+                    i++;
+                }else {
+                    alice+=piles[n];
+                    n--;
+                }
             }
+            turn =! turn;
         }
+
         return (alice>bob)?true:false;
     }
+
 }
