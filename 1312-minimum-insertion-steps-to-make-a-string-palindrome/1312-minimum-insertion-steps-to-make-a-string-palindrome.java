4f5638f4ace91c0e545d61a5ba60be27ab787ce1
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2025-12-15 23:00:26 +0530

    Time: 37 ms (11.3%), Space: 48.1 MB (6.76%) - LeetHub

---
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,44 @@
+// class Solution {
+//     public int minInsertions(String s) {
+//         int n = s.length();
+//         String str = new StringBuilder(s).reverse().toString();
+//         int ans = helper(s, str, n-1, n-1);
+//         return Math.abs(n-ans);
+//     }
+//     public int helper(String s1, String s2, int i, int j){
+//         if(i<0 || j<0) {
+//             return 0;
+//         }
+
+//         if(s1.charAt(i) == s2.charAt(j)) {
+//             return 1+helper(s1,s2,i-1,j-1);
+//         }else {
+//             return Math.max(helper(s1,s2,i-1,j), helper(s1,s2,i,j-1));
+//         }
+//     }
+// }
+
+
+
+
+class Solution {
+    public int minInsertions(String s) {
+        int n = s.length();
+        String str = new StringBuilder(s).reverse().toString();
+        Integer[][] dp = new Integer[n][n];
+        int ans = helper(s, str, n-1, n-1, dp);
+        return Math.abs(n-ans);
+    }
+    public int helper(String s1, String s2, int i, int j, Integer[][] dp){
+        if(i<0 || j<0) {
+            return 0;
+        }
+        if(dp[i][j] !=null) return dp[i][j];
+
+        if(s1.charAt(i) == s2.charAt(j)) {
+            return dp[i][j]=1+helper(s1,s2,i-1,j-1, dp);
+        }else {
+            return dp[i][j]= Math.max(helper(s1,s2,i-1,j,dp), helper(s1,s2,i,j-1,dp));
+        }
+    }
+}
