64cbee00f481b58496085162f38449be7923271e
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-03-24 21:29:35 +0530

    Time: 0 ms (100%), Space: 46.5 MB (55.67%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -32,41 +32,6 @@
 
 
 
-class Solution {
-    static int n;
-    public int maxProfit(int[] prices) {
-        n = prices.length;
-        int[][] dp = new int[n+1][2];
-        dp[n][0] = dp[n][1] = 0;
-
-        for(int idx=n-1; idx>=0; idx--){
-            for(int buy=0; buy<=1; buy++){
-                int profit = 0;
-                if(buy==1){
-                    int take = -prices[idx] + dp[idx+1][0];
-                    int notTake = 0 + dp[idx+1][1];
-                    profit = Math.max(take, notTake);
-                }else {
-                    int take = prices[idx] + dp[idx+1][1];
-                    int notTake = 0 + dp[idx+1][0];
-                    profit = Math.max(take, notTake);
-                }
-                dp[idx][buy] = profit;
-            }
-        }
-        return dp[0][1];
-    }
-}
-
-
-
-
-
-
-
-//space optimization
-
-
 // class Solution {
 //     static int n;
 //     public int maxProfit(int[] prices) {
@@ -92,3 +57,39 @@ class Solution {
 //         return dp[0][1];
 //     }
 // }
+
+
+
+
+
+
+
+//space optimization
+
+
+class Solution {
+    static int n;
+    public int maxProfit(int[] prices) {
+        n = prices.length;
+        int[] prev = new int[2];
+        int[] curr = new int[2];
+
+        for(int idx=n-1; idx>=0; idx--){
+            for(int buy=0; buy<=1; buy++){
+                int profit = 0;
+                if(buy==1){
+                    int take = -prices[idx] + prev[0];
+                    int notTake = 0 + prev[1];
+                    profit = Math.max(take, notTake);
+                }else {
+                    int take = prices[idx] + prev[1];
+                    int notTake = 0 + prev[0];
+                    profit = Math.max(take, notTake);
+                }
+                curr[buy] = profit;
+            }
+            prev = curr;
+        }
+        return prev[1];
+    }
+}
