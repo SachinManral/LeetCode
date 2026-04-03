1dad9272ac4fefbda68612a91f6f6dfb3471a9c4
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-04-04 02:46:29 +0530

    Time: 11 ms (96.65%), Space: 46.3 MB (77.33%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -54,31 +54,50 @@
 
 
 
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-        int n = coins.length;
-        int[][] dp = new int[n][amount+1];
+// class Solution {
+//     public int coinChange(int[] coins, int amount) {
+//         int n = coins.length;
+//         int[][] dp = new int[n][amount+1];
         
-        for(int i=0; i<=amount; i++) {
-            if(i%coins[0]==0) {
-                dp[0][i]=i/coins[0];
-            } else {
-                dp[0][i] = (int)1e9;
+//         for(int i=0; i<=amount; i++) {
+//             if(i%coins[0]==0) {
+//                 dp[0][i]=i/coins[0];
+//             } else {
+//                 dp[0][i] = (int)1e9;
+//             }
+//         }
+
+//         for(int i=1; i<n; i++) {
+//             for(int j=0; j<=amount; j++) {
+//                 int notTake = dp[i-1][j];
+//                 int take = (int)1e9;
+//                 if(coins[i]<=j) {
+//                     take = 1+dp[i][j-coins[i]];
+//                 }
+//                 dp[i][j] = Math.min(take, notTake);
+//             }
+
+//         }
+//         int ans = dp[n-1][amount];
+//         return ans>=(int)1e9?-1:ans;
+//     }
+// }
+
+
+
+
+class Solution {
+    public int coinChange(int[] coins, int x) {
+        int max = (int)1e9;
+        int[] dp = new int[x+1];
+        Arrays.fill(dp, max);
+
+        dp[0] = 0;
+        for(int c:coins) {
+            for(int j=c; j<=x; j++){
+                dp[j] = Math.min(dp[j], 1+dp[j-c]);
             }
         }
-
-        for(int i=1; i<n; i++) {
-            for(int j=0; j<=amount; j++) {
-                int notTake = dp[i-1][j];
-                int take = (int)1e9;
-                if(coins[i]<=j) {
-                    take = 1+dp[i][j-coins[i]];
-                }
-                dp[i][j] = Math.min(take, notTake);
-            }
-
-        }
-        int ans = dp[n-1][amount];
-        return ans>=(int)1e9?-1:ans;
+        return dp[x]>=max?-1:dp[x];
     }
 }
