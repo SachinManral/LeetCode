8049356f7a7723574a6ad0c44d192c79938cddff
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-04-08 12:23:44 +0530

    Time: 26 ms (30.67%), Space: 51.7 MB (19.24%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,31 +1,27 @@
 class Solution {
-    public int maximalRectangle(char[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        // stores length of horizontal 1s
-        int[][] dp = new int[m][n];
-        int max = 0;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                if(matrix[i][j] == '1'){
-                    if(j-1>= 0) {
-                        dp[i][j] = dp[i][j-1] + 1;
-                    } else {
-                        dp[i][j] = 1;
+    public int maximalRectangle(char[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int[][] dp= new int[n][m];
+        int max =0;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(mat[i][j]=='1'){
+                    if(j-1>=0){
+                        dp[i][j]=1+dp[i][j-1];
+                    }else {
+                        dp[i][j]=1;
                     }
-                } else {
-                    dp[i][j] = 0;
+                }else {
+                    dp[i][j]=0;
                 }
-
                 int minWidth = Integer.MAX_VALUE;
-                for(int k = i; k >= 0; k--) {
+                for(int k=i; k>=0; k--){
                     minWidth = Math.min(minWidth, dp[k][j]);
                     max = Math.max(max, minWidth*(i-k+1));
                 }
             }
         }
-
         return max;
     }
 }
