e09773ad753db9ff3942e138265ca0328ff42005
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-05-01 20:20:51 +0530

    Time: 1 ms (99.21%), Space: 46.6 MB (90.73%) - LeetHub

--- 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
+++ 0515-find-largest-value-in-each-tree-row/0515-find-largest-value-in-each-tree-row.java
@@ -13,26 +13,56 @@
  *     }
  * }
  */
+// class Solution {
+//     public List<Integer> largestValues(TreeNode root) {
+//         List<Integer> res = new ArrayList<>();
+//         if(root==null) return res;
+
+//         Queue<TreeNode> q = new LinkedList<>();
+//         q.offer(root);
+
+//         while(!q.isEmpty()){
+//             int size = q.size();
+//             int max = Integer.MIN_VALUE;
+
+//             while(size-- > 0){
+//                 TreeNode node = q.poll();
+//                 max = Math.max(node.val, max);
+//                 if(node.left != null) q.offer(node.left);
+//                 if(node.right!=null) q.offer(node.right);
+//             }
+//             res.add(max);
+//         }
+//         return res;
+//     }
+// }
+
+
+
+
+
+
+
+
 class Solution {
+    List<Integer> res;
     public List<Integer> largestValues(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        if(root==null) return res;
+        res = new ArrayList<>();
         
-        Queue<TreeNode> q = new LinkedList<>();
-        q.offer(root);
-
-        while(!q.isEmpty()){
-            int size = q.size();
-            int max = Integer.MIN_VALUE;
-
-            while(size-- > 0){
-                TreeNode node = q.poll();
-                max = Math.max(node.val, max);
-                if(node.left != null) q.offer(node.left);
-                if(node.right!=null) q.offer(node.right);
-            }
-            res.add(max);
-        }
+        dfs(root, 0);
         return res;
+    }
+
+    public void dfs(TreeNode root, int depth){
+        if(root==null) return;
+
+        if(depth==res.size()){
+            res.add(root.val);
+        }else {
+            res.set(depth, Math.max(res.get(depth), root.val));
+        }
+
+        dfs(root.left, depth+1);
+        dfs(root.right, depth+1);
     }
 }
