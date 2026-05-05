82ae70d4f98db4d1b29e9cde7e4f74ece5e91305
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-05-06 00:21:15 +0530

    Time: 0 ms (100%), Space: 46.6 MB (51.13%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -13,45 +13,95 @@ class Node {
 }
 */
 
+// class Solution {
+//     public Node copyRandomList(Node head) {
+//         if(head==null) return head;
+
+//         HashMap<Node, Node> map = new HashMap<>();
+//         Node curr = head;
+//         Node prev = null;
+//         Node newHead = null;
+
+//         while(curr!=null){
+//             Node temp = new Node(curr.val);
+
+//             map.put(curr, temp);
+
+//             if(newHead == null){
+//                 newHead = temp;
+//                 prev = newHead;
+//             }else{
+//                 prev.next = temp;
+//                 prev = temp;
+//             }
+//             curr = curr.next;
+//         }
+
+//         //fill random
+//         curr = head;
+//         Node newCurr = newHead;
+
+//         while(curr!=null){
+//             if(curr.random==null){
+//                 newCurr.random = null;
+//             }else{
+//                 // Node rNode = curr.random;
+//                 newCurr.random = map.get(curr.random);
+//             }
+
+//             curr = curr.next;
+//             newCurr = newCurr.next;
+//         }
+//         return newHead;
+//     }
+// }
+
+
+
+
+
+
+
 class Solution {
     public Node copyRandomList(Node head) {
         if(head==null) return head;
 
-        HashMap<Node, Node> map = new HashMap<>();
+        //Insert new Nodes b/w LL
         Node curr = head;
-        Node prev = null;
-        Node newHead = null;
-
         while(curr!=null){
-            Node temp = new Node(curr.val);
+            Node currNext = curr.next;
+            curr.next = new Node(curr.val);
+            curr.next.next = currNext;
 
-            map.put(curr, temp);
-
-            if(newHead == null){
-                newHead = temp;
-                prev = newHead;
-            }else{
-                prev.next = temp;
-                prev = temp;
-            }
-            curr = curr.next;
+            curr = currNext;
         }
 
-        //fill random
-        curr = head;
-        Node newCurr = newHead;
+        //Deep copy of Random pointers
+        curr= head;
+        while(curr!=null && curr.next !=null){
 
-        while(curr!=null){
             if(curr.random==null){
-                newCurr.random = null;
+                curr.next.random = null;
             }else{
-                // Node rNode = curr.random;
-                newCurr.random = map.get(curr.random);
+                curr.next.random = curr.random.next;
             }
 
+            curr = curr.next.next;
+        }
+
+        //seperate LL
+        Node newHead = head.next;
+        Node newCurr = newHead;
+        curr = head;
+
+        while(curr!=null && newCurr!=null){
+            curr.next = curr.next==null?null:curr.next.next;
+            newCurr.next = newCurr.next==null?null:newCurr.next.next;
+            
             curr = curr.next;
             newCurr = newCurr.next;
         }
+
         return newHead;
     }
 }
