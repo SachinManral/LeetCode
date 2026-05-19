e407830a7af67de9b62c83eadd8b8af632d10e49
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-05-19 11:10:55 +0530

    Time: 2 ms (88.98%), Space: 80.4 MB (63.48%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,20 +1,36 @@
+// class Solution {
+//     public int getCommon(int[] nums1, int[] nums2) {
+//         HashSet<Integer> set = new HashSet<>();
+//         for(int i=0; i<nums1.length; i++){
+//             set.add(nums1[i]);
+//         }
+
+//         for(int i=0; i<nums2.length; i++){
+//             if(set.contains(nums2[i])){
+//                 return nums2[i];
+//             }
+//         }
+
+//         return -1;
+//     }
+// }
+
+
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        for(int i=0; i<nums1.length; i++){
-            set.add(nums1[i]);
-        }
+        int i=0, j=0;
 
-        for(int i=0; i<nums2.length; i++){
-            if(set.contains(nums2[i])){
-                return nums2[i];
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]==nums2[j]){
+                return nums1[i];
+            }
+
+            if(nums1[i]<nums2[j]){
+                i++;
+            }else {
+                j++;
             }
         }
-
         return -1;
     }
 }
-
-
-// class Solution {
-//     public int getCommon(int[] nums1, int[] nums2) {
