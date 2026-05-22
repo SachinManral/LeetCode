280c37ecf9388b4fdb686c5e360399782a536eac
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-05-22 10:38:16 +0530

    Time: 300 ms (23.1%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,9 +1,25 @@
 # Write your MySQL query statement below
-select (
-    select max(salary)
-    from Employee 
-    Where salary < (Select Max(salary)
-    from Employee)
-) as SecondHighestSalary;
+-- select max(salary) as SecondHighestSalary 
+-- from Employee
+-- where salary < (
+--             Select max(salary)
+--             from Employee
+--             )
+
+-- SELECT (
+--     SELECT salary FROM (
+--         SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) rnk
+--         FROM Employee
+--     ) t
+--     WHERE rnk = 2
+--     LIMIT 1
+-- ) SecondHighestSalary;
 
 
+
+with secHighest as (
+    select max(salary) as SecondHighestSalary from Employee
+    where salary < (select max(salary) from Employee)
+)
+
+select * from secHighest
