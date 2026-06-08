81cb6ca3b8346be636a84ea1c6f1837eabdd6bfb
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-06-08 20:44:07 +0530

    Time: 4 ms (99.24%), Space: 159.8 MB (73.16%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,18 +1,33 @@
 // class Solution {
 //     public int[] pivotArray(int[] nums, int pivot) {
-//         int pivotIdx = -1;
-//         for(int i=0; i<nums.length; i++){
-//             if(nums[i]==pivot){
-//                 pivotIdx = i;
-//                 break;
-//             }
+//         int n = nums.length;
+//         ArrayList<Integer> small = new ArrayList<>();
+//         ArrayList<Integer> large = new ArrayList<>();
+//         ArrayList<Integer> equal = new ArrayList<>();
+
+//         for(int i=0; i<n; i++){
+//             if(nums[i]<pivot) small.add(nums[i]);
+//             else if(nums[i]>pivot) large.add(nums[i]);
+//             else equal.add(nums[i]);
 //         }
 
-//         for(int i=0; i<nums.length; i++){
-
+//         int idx=0;
+//         for(int i=0; i<small.size(); i++){
+//             nums[idx++] = small.get(i);
 //         }
+
+//         for(int i=0; i<equal.size(); i++){
+//             nums[idx++] = equal.get(i);
+//         }
+
+//         for(int i=0; i<large.size(); i++){
+//             nums[idx++] = large.get(i);
+//         }
+
+//         return nums;
 //     }
 // }
+
 
 
 
@@ -20,29 +35,30 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
         int n = nums.length;
-        ArrayList<Integer> small = new ArrayList<>();
-        ArrayList<Integer> large = new ArrayList<>();
-        ArrayList<Integer> equal = new ArrayList<>();
+        int left = 0;
+        int right = n-1;
 
-        for(int i=0; i<n; i++){
-            if(nums[i]<pivot) small.add(nums[i]);
-            else if(nums[i]>pivot) large.add(nums[i]);
-            else equal.add(nums[i]);
+        int i=0;
+        int j=n-1;
+        
+        int[] res = new int[n];
+        while(i<n){
+            if(nums[i]<pivot){
+                res[left++] = nums[i];
+            }
+
+            if(nums[j]>pivot){
+                res[right--] = nums[j];
+            }
+
+            i++;
+            j--; 
         }
 
-        int idx=0;
-        for(int i=0; i<small.size(); i++){
-            nums[idx++] = small.get(i);
+        //remaining index contains pivot
+        while(left<=right){
+            res[left++] = pivot;
         }
-
-        for(int i=0; i<equal.size(); i++){
-            nums[idx++] = equal.get(i);
-        }
-
-        for(int i=0; i<large.size(); i++){
-            nums[idx++] = large.get(i);
-        }
-
-        return nums;
+        return res;
     }
 }
