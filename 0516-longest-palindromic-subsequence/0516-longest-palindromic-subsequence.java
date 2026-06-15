aa6d391015f70f587e0e1189b0e7cb707bc755c2
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-06-15 20:13:39 +0530

    Time: 47 ms (46.42%), Space: 118.1 MB (5.13%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -1,68 +1,22 @@
-// class Solution {
-//     public int longestPalindromeSubseq(String s) {
-//         String str = new StringBuilder(s).reverse().toString();
-//         return helper(s, str, s.length()-1, s.length()-1);
-//     }
-//     public int helper(String s1, String s2, int i, int j){
-//         if(i<0 || j<0) {
-//             return 0;
-//         }
-
-//         if(s1.charAt(i) == s2.charAt(j)) {
-//             return 1+helper(s1,s2,i-1,j-1);
-//         }else {
-//             return Math.max(helper(s1,s2,i-1,j), helper(s1,s2,i,j-1));
-//         }
-//     }
-// }
-
-
-
-
-// class Solution {
-//     public int longestPalindromeSubseq(String s) {
-//         String str = new StringBuilder(s).reverse().toString();
-//         Integer[][] dp = new Integer[s.length()][s.length()];
-//         return helper(s, str, s.length()-1, s.length()-1,dp);
-//     }
-//     public int helper(String s1, String s2, int i, int j, Integer[][] dp){
-//         if(i<0 || j<0) {
-//             return 0;
-//         }
-//         if(dp[i][j] != null) return dp[i][j];
-
-//         if(s1.charAt(i) == s2.charAt(j)) {
-//             return dp[i][j]= 1+helper(s1,s2,i-1,j-1,dp);
-//         }else {
-//             return dp[i][j] = Math.max(helper(s1,s2,i-1,j,dp), helper(s1,s2,i,j-1,dp));
-//         }
-//     }
-// }
-
-
-
-
 class Solution {
+    Integer[][] dp;
     public int longestPalindromeSubseq(String s) {
         String str = new StringBuilder(s).reverse().toString();
-        int n = s.length();
-        Integer[][] dp = new Integer[n+1][n+1];
-        for(int i=0; i<=n; i++) {
-            dp[0][i] = 0;
-        }
-        for(int i=0; i<=n; i++) {
-            dp[i][0] = 0;
+        dp = new Integer[s.length()][s.length()];
+        return helper(s, str, s.length()-1, str.length()-1);
+    }
+
+    public int helper(String s1, String s2, int i, int j){
+        if(i<0 || j<0){
+            return 0;
         }
 
-        for(int i=1; i<=n; i++) {
-            for(int j=1; j<=n; j++) {
-                if(s.charAt(i-1) == str.charAt(j-1)) {
-                    dp[i][j]= 1+dp[i-1][j-1];
-                }else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
-            }
+        if(dp[i][j]!=null) return dp[i][j];
+
+        if(s1.charAt(i)==s2.charAt(j)){
+            return dp[i][j]= 1+helper(s1, s2, i-1, j-1);
+        }else{
+            return dp[i][j]= Math.max(helper(s1, s2, i-1, j), helper(s1, s2, i, j-1));
         }
-        return dp[n][n];
     }
 }
