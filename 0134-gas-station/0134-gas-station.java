9a48abf69d8e77eac5e866c27a4bf3eae745f509
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-07-13 22:03:48 +0530

    Time: 2 ms (99.27%), Space: 119.2 MB (91.94%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -1,16 +1,44 @@
+// //T.C - O(n^2);
+// class Solution {
+//     public int canCompleteCircuit(int[] gas, int[] cost) {
+//         int n = gas.length;
+//         for(int i=0; i<n; i++){
+//             int gain=0;
+//             int count=0;
+//             int j=i;
+
+//             while(count<n){
+//                 gain += gas[j]-cost[j];
+//                 if(gain<0) break;
+
+//                 j = (j+1)%n;
+//                 count++;
+//             }
+//             if(count==n) return i;
+//         }
+//         return -1;
+//     }
+// }
+
+
+
+
+
+//T.C-O(n^2)
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
-        int bal=0;
+        int gain=0;
         int need=0;
         int st=0;
+
         for(int i=0; i<gas.length; i++){
-            bal+=gas[i]-cost[i];
-            if(bal<0){
-                need+=bal;
+            gain += gas[i]-cost[i];
+            if(gain<0){
+                need+=gain;
                 st=i+1;
-                bal=0;
+                gain=0;
             }
         }
-        return bal+need>=0?st:-1;
+        return gain+need>=0?st:-1;
     }
 }
