6993466c05ae09f4bf3feedabf3193112035e7f4
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-08-06 15:46:27 +0530

    Time: 576 ms (73.06%), Space: 0B (100%) - LeetHub

---
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -0,0 +1,29 @@
+# Write your MySQL query statement below
+select round(sum(tiv_2016),2) as tiv_2016
+from Insurance
+where tiv_2015 in(
+    select tiv_2015
+    from Insurance
+    group by tiv_2015
+    having count(*)>1
+) and 
+    (lat, lon) in(
+    select lat, lon
+    from Insurance
+    group by lat, lon
+    having count(*)=1
+)
+
+
+
+
+
+-- with uniq_cords as (
+--     select * 
+--     count(*) over (partition by lat, lon) as attempts,
+--     count(*) over (partition by tiv_2015) as tivs
+--     from Insurance
+--     order by pid
+-- )
+
+-- Select round(cast)
