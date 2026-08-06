4ba7f520ec26e66b98937d6acd0089d3b006a62e
Sachin Manral <168269786+SachinManral@users.noreply.github.com>
2026-08-06 15:49:38 +0530

    Time: 786 ms (12.49%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,29 +1,28 @@
 # Write your MySQL query statement below
-select round(sum(tiv_2016),2) as tiv_2016
-from Insurance
-where tiv_2015 in(
-    select tiv_2015
-    from Insurance
-    group by tiv_2015
-    having count(*)>1
-) and 
-    (lat, lon) in(
-    select lat, lon
-    from Insurance
-    group by lat, lon
-    having count(*)=1
-)
-
-
-
-
-
--- with uniq_cords as (
---     select * 
---     count(*) over (partition by lat, lon) as attempts,
---     count(*) over (partition by tiv_2015) as tivs
+-- select round(sum(tiv_2016),2) as tiv_2016
+-- from Insurance
+-- where tiv_2015 in(
+--     select tiv_2015
 --     from Insurance
---     order by pid
+--     group by tiv_2015
+--     having count(*)>1
+-- ) and 
+--     (lat, lon) in(
+--     select lat, lon
+--     from Insurance
+--     group by lat, lon
+--     having count(*)=1
 -- )
 
--- Select round(cast)
+
+
+
+
+with cte as (
+    select *, 
+    count(*) over (partition by lat, lon) as attempts,
+    count(*) over (partition by tiv_2015) as tivs
+    from Insurance
+)
+Select round(sum(tiv_2016),2) as tiv_2016
+from cte where attempts=1 and tivs>1
